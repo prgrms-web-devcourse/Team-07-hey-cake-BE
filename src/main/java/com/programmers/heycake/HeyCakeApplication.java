e6d5b5f0c686bce548cc16e017fb9bf11e864633
권성준 <tjdwns1865@naver.com>
2023-02-14 14:16:50 +0900

    Init: 프로젝트 init

---
+++ src/main/java/com/programmers/heycake/HeyCakeApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.heycake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmers/heycake/HeyCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.heycake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
