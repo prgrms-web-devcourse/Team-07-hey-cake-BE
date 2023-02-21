90dd3ec52d19a9e8a500e211aab200803e989460
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-02-21 21:40:08 +0900

    Feat: 공통 예외 처리 구현 (#19)

---
+++ src/main/java/com/programmers/heycake/common/dto/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.programmers.heycake.common.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public record ErrorResponse(
+		String message,
+		String path,
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+		LocalDateTime time,
+		List<FieldError> inputErrors
+) {
+
+	public record FieldError(String field, String rejectedValue, String message) {
+	}
+
+	public static ErrorResponse of(String message, String path, List<FieldError> inputErrors) {
+		return new ErrorResponse(message, path, LocalDateTime.now(), inputErrors);
+	}
+}
