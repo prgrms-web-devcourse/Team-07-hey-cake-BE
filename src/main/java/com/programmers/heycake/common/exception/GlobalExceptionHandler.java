35c51b1ef163ffa3ab6862c73fe467d5b8db293c
권성준 <tjdwns1865@naver.com>
2023-02-21 18:54:12 +0900

    Style: 메서드명 명확하게 수정

--- src/main/java/com/programmers/heycake/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/heycake/common/exception/GlobalExceptionHandler.java
@@ -19,14 +19,14 @@ import com.programmers.heycake.common.dto.ErrorResponse;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<ErrorResponse> badRequestHandler(HttpServletRequest request) {
+	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(HttpServletRequest request) {
 		return ResponseEntity
 				.badRequest()
 				.body(ErrorResponse.of(BAD_REQUEST.getMessage(), request.getRequestURI(), null));
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorResponse> badRequestHandler(
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
 			HttpServletRequest request,
 			MethodArgumentNotValidException e
 	) {
@@ -36,26 +36,26 @@ public class GlobalExceptionHandler {
 						ErrorResponse.of(
 								BAD_REQUEST.getMessage(),
 								request.getRequestURI(),
-								getFieldErrors(e.getBindingResult())
+								makeFieldErrors(e.getBindingResult())
 						)
 				);
 	}
 
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<ErrorResponse> runtimeExceptionHandler(HttpServletRequest request, BusinessException e) {
+	public ResponseEntity<ErrorResponse> handleBusinessException(HttpServletRequest request, BusinessException e) {
 		return ResponseEntity
 				.status(e.getErrorCode().getStatus())
 				.body(ErrorResponse.of(e.getErrorCode().getMessage(), request.getRequestURI(), null));
 	}
 
 	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<ErrorResponse> runtimeExceptionHandler(HttpServletRequest request, RuntimeException e) {
+	public ResponseEntity<ErrorResponse> handleRuntimeException(HttpServletRequest request, RuntimeException e) {
 		return ResponseEntity
 				.badRequest()
 				.body(ErrorResponse.of(e.getMessage(), request.getRequestURI(), null));
 	}
 
-	private List<ErrorResponse.FieldError> getFieldErrors(BindingResult bindingResult) {
+	private List<ErrorResponse.FieldError> makeFieldErrors(BindingResult bindingResult) {
 
 		return bindingResult.getFieldErrors()
 				.stream()
