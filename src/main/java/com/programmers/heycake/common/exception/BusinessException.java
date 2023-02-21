898bc936629405a904d9aca5c6fb87717ea66e88
권성준 <tjdwns1865@naver.com>
2023-02-21 18:36:09 +0900

    Refactor: 변경되지 않을 필드에 final 키워드 추가

--- src/main/java/com/programmers/heycake/common/exception/BusinessException.java
+++ src/main/java/com/programmers/heycake/common/exception/BusinessException.java
@@ -1,14 +1,11 @@
 package com.programmers.heycake.common.exception;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BusinessException extends RuntimeException {
 
-	private ErrorCode errorCode;
+	private final ErrorCode errorCode;
 
 	public BusinessException(ErrorCode errorCode) {
 		this.errorCode = errorCode;
