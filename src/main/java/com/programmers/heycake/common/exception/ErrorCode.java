a8119b69a1cafa0f4f6e0963872844581e18d46f
권성준 <tjdwns1865@naver.com>
2023-02-23 23:21:22 +0900

    Fix: ErrorCode 에 미인증, 미인가 예외 코드 추가

--- src/main/java/com/programmers/heycake/common/exception/ErrorCode.java
+++ src/main/java/com/programmers/heycake/common/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 입력 값입니다.");
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 입력 값입니다."),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
+	FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없는 사용자입니다.");
 
 	private final HttpStatus status;
 	private final String message;
