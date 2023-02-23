0effff917cac7f36ad8ee24a4d7ca4290e769f4a
goseungwon <knmy0925@gmail.com>
2023-02-24 05:08:16 +0900

    Test: Security 기능 테스트 작성

---
+++ src/test/java/com/programmers/heycake/common/config/OAuth2SuccessHandlerTest.java
@@ -0,0 +1,122 @@
+package com.programmers.heycake.common.config;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.programmers.heycake.TestUtils;
+import com.programmers.heycake.domain.member.model.Member;
+import com.programmers.heycake.domain.member.model.Token;
+import com.programmers.heycake.domain.member.repository.MemberRepository;
+import com.programmers.heycake.domain.member.repository.TokenRepository;
+
+@Transactional
+@SpringBootTest
+class OAuth2SuccessHandlerTest {
+
+	@Autowired
+	private OAuth2SuccessHandler oAuth2SuccessHandler;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private TokenRepository tokenRepository;
+
+	HttpServletRequest request = mock(HttpServletRequest.class);
+	HttpServletResponse response = mock(HttpServletResponse.class);
+	PrintWriter printWriter = mock(PrintWriter.class);
+	Authentication authentication;
+	Member member = TestUtils.createMember();
+
+	@BeforeEach
+	void setUp() {
+		// Principal
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("email", member.getEmail());
+		attributes.put("birthday", member.getBirth());
+		attributes.put("profileImage", member.getImageUrl());
+		attributes.put("nickname", member.getNickname());
+		attributes.put("key", "key");
+
+		List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+		OAuth2User oAuth2User = new DefaultOAuth2User(authorities, attributes, "key");
+
+		// Aauthentication
+		authentication = new UsernamePasswordAuthenticationToken(oAuth2User, null, authorities);
+
+		SecurityContext context = SecurityContextHolder.getContext();
+		context.setAuthentication(authentication);
+	}
+
+	@Nested
+	@DisplayName("onAuthenticationSuccess")
+	@Transactional
+	class OnAuthenticationSuccess {
+
+		@Test
+		@DisplayName("Success - 이미 존재하는 회원인 경우 회원가입(signup) 없이 토큰 저장 - onAuthenticationSuccess")
+		void onAuthenticationSuccessWhenExistsMember() throws Exception {
+			// given
+			memberRepository.save(member);
+
+			when(response.getWriter())
+					.thenReturn(printWriter);
+
+			// when
+			oAuth2SuccessHandler.onAuthenticationSuccess(request, response, authentication);
+
+			// then
+			List<Token> tokens = tokenRepository.findAll();
+			assertThat(tokens).hasSize(1);
+			assertThat(tokens.get(0).getEmail()).isEqualTo(member.getEmail());
+		}
+
+		@Test
+		@DisplayName("Success - 존재하지 않는 회원인 경우 회원가입(signup) 후 토큰 저장 - onAuthenticationSuccess")
+		void onAuthenticationSuccessWhenNotExistsMember() throws Exception {
+			// given
+			when(response.getWriter())
+					.thenReturn(printWriter);
+
+			// when
+			oAuth2SuccessHandler.onAuthenticationSuccess(request, response, authentication);
+
+			// then
+			List<Member> members = memberRepository.findAll();
+			List<Token> tokens = tokenRepository.findAll();
+
+			assertThat(members).hasSize(1);
+			assertThat(members.get(0))
+					.usingRecursiveComparison()
+					.ignoringFields("id")
+					.isEqualTo(member);
+			assertThat(tokens).hasSize(1);
+			assertThat(tokens.get(0).getEmail()).isEqualTo(member.getEmail());
+		}
+	}
+
+}
---
+++ src/test/java/com/programmers/heycake/domain/member/service/CustomOauth2UserServiceTest.java
@@ -0,0 +1,97 @@
+package com.programmers.heycake.domain.member.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@ExtendWith(MockitoExtension.class)
+class CustomOauth2UserServiceTest {
+	@InjectMocks
+	private CustomOauth2UserService customOauth2UserService;
+
+	@Mock
+	private DefaultOAuth2UserService defaultOAuth2UserService;
+
+	@Nested
+	@DisplayName("loadUser")
+	class LoadUser {
+
+		@Test
+		@DisplayName("Fail - userRequest가 비어있다면 Exception을 던진다. - loadUser")
+		void loadUserUserRequestFail() {
+			//given
+			OAuth2UserRequest userRequest = null;
+
+			//when //then
+			assertThrows(IllegalArgumentException.class, () -> customOauth2UserService.loadUser(userRequest));
+		}
+
+		@Test
+		@DisplayName("Success - OAuth2UserRequest를 OAuth2User로 변환한다. - loadUser")
+		void loadUserToMapSuccess() {
+			//given
+			ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("kakao").build();
+			OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(null, null, null, null);
+			Map<String, Object> profile = new HashMap<>();
+			profile.put("nickname", "nickname");
+			profile.put("profile_image_url", "profileImage");
+			Map<String, Object> account = new HashMap<>();
+			account.put("email", "email");
+			account.put("birthday", "birthday");
+			Map<String, Object> attributes = new HashMap<>();
+			attributes.put("profile", profile);
+			attributes.put("kakao_account", account);
+			attributes.put("key", "key");
+
+			OAuth2User oAuth2User = new OAuth2User() {
+				@Override
+				public Map<String, Object> getAttributes() {
+					return attributes;
+				}
+
+				@Override
+				public Collection<? extends GrantedAuthority> getAuthorities() {
+					return null;
+				}
+
+				@Override
+				public String getName() {
+					return null;
+				}
+			};
+
+			OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
+
+			//when
+			when(defaultOAuth2UserService.loadUser(oAuth2UserRequest))
+					.thenReturn(oAuth2User);
+
+			//then
+			Map<String, Object> resultAttributes = customOauth2UserService.loadUser(oAuth2UserRequest).getAttributes();
+			assertThat(resultAttributes)
+					.contains(
+							entry("profile", profile),
+							entry("kakao_account", account),
+							entry("key", "key")
+					);
+		}
+	}
+}
