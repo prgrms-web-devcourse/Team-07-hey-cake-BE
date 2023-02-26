7b7a02de9e1263afd3147b4b82a617929cff4ec3
권성준 <tjdwns1865@naver.com>
2023-02-26 15:33:06 +0900

    Feat: @TransactionalEventListener 사용하여 S3 업로드/삭제 롤백 구현

--- src/main/java/com/programmers/heycake/domain/image/service/ImageIntegrationService.java
+++ src/main/java/com/programmers/heycake/domain/image/service/ImageIntegrationService.java
@@ -1,9 +1,13 @@
 package com.programmers.heycake.domain.image.service;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.programmers.heycake.domain.image.event.DeleteEvent;
+import com.programmers.heycake.domain.image.event.UploadRollbackEvent;
+import com.programmers.heycake.domain.image.model.entity.Image;
 import com.programmers.heycake.domain.image.model.vo.ImageType;
 
 import lombok.RequiredArgsConstructor;
@@ -14,17 +18,20 @@ public class ImageIntegrationService {
 
 	private final ImageUploadService imageUploadService;
 	private final ImageService imageService;
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	@Transactional
 	public void createAndUploadImage(MultipartFile multipartFile, String subPath, Long referenceId, ImageType imageType) {
 		String savedUrl = imageUploadService.upload(multipartFile, subPath);
-		imageService.createImage(referenceId, imageType, savedUrl);
+		Image image = new Image(referenceId, imageType, savedUrl);
+		applicationEventPublisher.publishEvent(new UploadRollbackEvent(subPath, image.getFilename()));
+		imageService.createImage(image);
 	}
 
 	@Transactional
 	public void deleteImage(Long referenceId, ImageType imageType, String subPath, String savedFilename) {
 		imageService.deleteImage(referenceId, imageType);
-		imageUploadService.delete(subPath, savedFilename);
+		applicationEventPublisher.publishEvent(new DeleteEvent(subPath, savedFilename));
 	}
 
 }
