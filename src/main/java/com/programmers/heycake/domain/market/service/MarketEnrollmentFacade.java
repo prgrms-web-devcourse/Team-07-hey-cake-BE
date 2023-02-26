f918663ad31c33ad06e0e54483931f4e4e6df008
권성준 <tjdwns1865@naver.com>
2023-02-26 16:57:23 +0900

    Refactor: ImageIntegrationService 도입에 따른 MarketEnrollmentFacade 구조 개선

--- src/main/java/com/programmers/heycake/domain/market/service/MarketEnrollmentFacade.java
+++ src/main/java/com/programmers/heycake/domain/market/service/MarketEnrollmentFacade.java
@@ -5,8 +5,7 @@ import static com.programmers.heycake.domain.image.model.vo.ImageType.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.programmers.heycake.domain.image.service.ImageS3Service;
-import com.programmers.heycake.domain.image.service.ImageService;
+import com.programmers.heycake.domain.image.service.ImageIntegrationService;
 import com.programmers.heycake.domain.market.model.dto.MarketEnrollmentRequest;
 
 import lombok.RequiredArgsConstructor;
@@ -18,18 +17,23 @@ public class MarketEnrollmentFacade {
 	private static final String ENROLLMENT_IMAGE_PATH = "image/marketEnrollment";
 
 	private final MarketEnrollmentService marketEnrollmentService;
-	private final ImageS3Service imageS3Service;
-	private final ImageService imageService;
+	private final ImageIntegrationService imageIntegrationService;
 
 	@Transactional
 	public Long enrollMarket(MarketEnrollmentRequest request) {
 
 		Long enrollmentId = marketEnrollmentService.enrollMarket(request);
 
-		String licenseImgUrl = imageS3Service.upload(request.businessLicenseImage(), ENROLLMENT_IMAGE_PATH);
-		String marketImgUrl = imageS3Service.upload(request.marketImage(), ENROLLMENT_IMAGE_PATH);
-		imageService.createImage(enrollmentId, ENROLLMENT_LICENSE, licenseImgUrl);
-		imageService.createImage(enrollmentId, ENROLLMENT_MARKET, marketImgUrl);
+		imageIntegrationService.createAndUploadImage(
+				request.businessLicenseImage(),
+				ENROLLMENT_IMAGE_PATH, enrollmentId,
+				ENROLLMENT_LICENSE
+		);
+		imageIntegrationService.createAndUploadImage(
+				request.marketImage(),
+				ENROLLMENT_IMAGE_PATH, enrollmentId,
+				ENROLLMENT_MARKET
+		);
 
 		return enrollmentId;
 	}
