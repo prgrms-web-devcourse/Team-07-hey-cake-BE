7208ef6e0e9ede115cdf75917cee58a63cd16254
Kong <rhdtn311@naver.com>
2023-02-27 05:26:20 +0900

    Chore: @Repository 어노테이션 제거

--- src/main/java/com/programmers/heycake/domain/market/repository/MarketEnrollmentRepository.java
+++ src/main/java/com/programmers/heycake/domain/market/repository/MarketEnrollmentRepository.java
@@ -1,10 +1,8 @@
 package com.programmers.heycake.domain.market.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.programmers.heycake.domain.market.model.entity.MarketEnrollment;
 
-@Repository
 public interface MarketEnrollmentRepository extends JpaRepository<MarketEnrollment, Long> {
 }
