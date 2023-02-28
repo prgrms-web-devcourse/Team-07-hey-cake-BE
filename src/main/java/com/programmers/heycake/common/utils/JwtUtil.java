a6997eebf9daaa1cbc0ea5a86399592320219936
Kong <rhdtn311@naver.com>
2023-02-28 13:37:54 +0900

    Feat: 멤버 검증 기능 구현

---
+++ src/main/java/com/programmers/heycake/common/utils/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.programmers.heycake.common.utils;
+
+import java.util.Objects;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JwtUtil {
+	public static Long getMemberId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
+			throw new AccessDeniedException("잘못된 접근입니다.");
+		}
+		return (Long)authentication.getPrincipal();
+	}
+
+	public static boolean isValidAccess(Long memberId) {
+		return Objects.equals(JwtUtil.getMemberId(), memberId);
+	}
+}
