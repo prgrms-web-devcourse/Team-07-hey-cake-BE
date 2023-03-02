c74335b9fd2b1a0f67cc292d2b517002ef31d6c2
bufflect <bndgc@naver.com>
2023-03-03 01:21:40 +0900

    Feat: Security 예외처리 추가

---
+++ src/main/java/com/programmers/heycake/common/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,35 @@
+package com.programmers.heycake.common.exception;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.programmers.heycake.common.dto.ErrorResponse;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException {
+
+		ErrorResponse errorResponse = ErrorResponse.of("권한이 없습니다.", "path", Collections.emptyList());
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+		OutputStream outputStream = response.getOutputStream();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.writeValue(outputStream, errorResponse);
+
+		outputStream.flush();
+		outputStream.close();
+	}
+}
