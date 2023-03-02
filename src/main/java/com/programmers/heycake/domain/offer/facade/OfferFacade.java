adf22d1cee7beb22970c893597c1c67e9983c1d0
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-03-03 04:27:54 +0900

    Feat: 제안(스레드) 생성 기능 구현 (#81)

---
+++ src/main/java/com/programmers/heycake/domain/offer/facade/OfferFacade.java
@@ -0,0 +1,36 @@
+package com.programmers.heycake.domain.offer.facade;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.programmers.heycake.domain.image.model.vo.ImageType;
+import com.programmers.heycake.domain.image.service.ImageIntegrationService;
+import com.programmers.heycake.domain.offer.model.dto.request.OfferSaveRequest;
+import com.programmers.heycake.domain.offer.service.OfferService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OfferFacade {
+
+	private static final String OFFER_IMAGE_SUB_PATH = "images/offers";
+
+	private final OfferService offerService;
+	private final ImageIntegrationService imageIntegrationService;
+
+	@Transactional
+	public Long saveOffer(OfferSaveRequest offerSaveRequest, Long memberId) {
+
+		// TODO : 회원 검증 로직
+
+		Long savedOfferId = offerService.saveOffer(memberId, offerSaveRequest.orderId(), offerSaveRequest.expectedPrice(),
+				offerSaveRequest.content());
+
+		imageIntegrationService.createAndUploadImage(offerSaveRequest.offerImage(), OFFER_IMAGE_SUB_PATH, savedOfferId,
+				ImageType.OFFER);
+
+		return savedOfferId;
+	}
+
+}
