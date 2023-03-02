eaceae31077d66ba9ffeff2cbd31d9e133c64c41
bufflect <bndgc@naver.com>
2023-03-03 06:13:26 +0900

    Refactor: OrderFacade for문 sream으로 구조 변경

--- src/main/java/com/programmers/heycake/domain/order/facade/OrderFacade.java
+++ src/main/java/com/programmers/heycake/domain/order/facade/OrderFacade.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.programmers.heycake.domain.image.service.ImageIntegrationService;
-import com.programmers.heycake.domain.image.service.ImageService;
-import com.programmers.heycake.domain.offer.facade.OfferFacade;
 import com.programmers.heycake.domain.order.model.dto.request.OrderCreateRequest;
 import com.programmers.heycake.domain.order.service.OrderService;
 
@@ -21,21 +19,20 @@ public class OrderFacade {
 	private static final String ORDER_IMAGE_SUB_PATH = "images/orders";
 
 	private final ImageIntegrationService imageIntegrationService;
-	private final ImageService imageService;
-	private final OfferFacade offerFacade;
 
 	@Transactional
 	public void createOrder(OrderCreateRequest orderCreateRequest) {
 		Long orderId = orderService.create(orderCreateRequest);
-		if (orderCreateRequest.cakeImages() != null) {
-			for (int i = 0; i < orderCreateRequest.cakeImages().size(); i++) {
-				imageIntegrationService.createAndUploadImage(
-						orderCreateRequest.cakeImages().get(i),
-						ORDER_IMAGE_SUB_PATH,
-						orderId,
-						ORDER
-				);
-			}
-		}
+
+		orderCreateRequest.cakeImages()
+				.forEach(
+						cakeImage ->
+								imageIntegrationService.createAndUploadImage(
+										cakeImage,
+										ORDER_IMAGE_SUB_PATH,
+										orderId,
+										ORDER
+								))
+		;
 	}
 }
