801f6708a1636f57e47b7ef384ad2211c86ededb
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-03-03 04:27:54 +0900

    Feat: 제안(스레드) 생성 기능 구현 (#81)

--- src/main/java/com/programmers/heycake/domain/market/repository/MarketRepository.java
+++ src/main/java/com/programmers/heycake/domain/market/repository/MarketRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.programmers.heycake.domain.market.model.entity.Market;
+import com.programmers.heycake.domain.member.model.entity.Member;
 
 public interface MarketRepository extends JpaRepository<Market, Long> {
+	Optional<Market> findByMember(Member member);
 
 	@Query("SELECT m FROM Market m JOIN FETCH m.marketEnrollment WHERE m.id = :marketId")
 	Optional<Market> findByIdFetchWithMarketEnrollment(@Param("marketId") Long marketId);
