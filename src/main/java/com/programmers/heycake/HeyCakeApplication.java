73207494f2d33eba3229497ca1bb0574ceacb316
bufflect <bndgc@naver.com>
2023-03-02 23:56:25 +0900

    Chore: configuration-processor 추가

--- src/main/java/com/programmers/heycake/HeyCakeApplication.java
+++ src/main/java/com/programmers/heycake/HeyCakeApplication.java
@@ -2,8 +2,10 @@ package com.programmers.heycake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class HeyCakeApplication {
 
 	public static void main(String[] args) {
