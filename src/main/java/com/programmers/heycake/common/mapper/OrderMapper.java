b773b1bacba6874a1c2b28cb62fa0e2de08bc386
knmy0925@gmail.com <85796588+goseungwon@users.noreply.github.com>
2023-03-03 18:02:07 +0900

    Feat: 업체 선택 완료 API 구현 (#49)

--- src/main/java/com/programmers/heycake/common/mapper/OrderMapper.java
+++ src/main/java/com/programmers/heycake/common/mapper/OrderMapper.java
@@ -2,16 +2,43 @@ package com.programmers.heycake.common.mapper;
 
 import static com.programmers.heycake.common.util.AuthenticationUtil.*;
 import static com.programmers.heycake.domain.order.model.vo.OrderStatus.*;
-import static lombok.AccessLevel.*;
 
+import com.programmers.heycake.domain.order.model.dto.OrderDto;
 import com.programmers.heycake.domain.order.model.dto.request.OrderCreateRequest;
 import com.programmers.heycake.domain.order.model.entity.CakeInfo;
 import com.programmers.heycake.domain.order.model.entity.Order;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class OrderMapper {
+
+	public static Order toEntity(OrderDto orderDto) {
+		return Order.builder()
+				.memberId(orderDto.memberId())
+				.title(orderDto.title())
+				.orderStatus(orderDto.orderStatus())
+				.hopePrice(orderDto.hopePrice())
+				.region(orderDto.region())
+				.visitDate(orderDto.visitDate())
+				.cakeInfo(orderDto.cakeInfo())
+				.build();
+	}
+
+	public static OrderDto toOrderDto(Order order) {
+		return OrderDto.builder()
+				.id(order.getId())
+				.memberId(order.getMemberId())
+				.title(order.getTitle())
+				.orderStatus(order.getOrderStatus())
+				.hopePrice(order.getHopePrice())
+				.region(order.getRegion())
+				.visitDate(order.getVisitDate())
+				.cakeInfo(order.getCakeInfo())
+				.build();
+	}
+
 	public static Order toEntity(OrderCreateRequest orderCreateRequest, CakeInfo cakeInfo) {
 		return Order.builder()
 				.cakeInfo(cakeInfo)
---
+++ src/main/java/com/programmers/heycake/domain/order/model/dto/request/HistoryControllerRequest.java
@@ -0,0 +1,14 @@
+package com.programmers.heycake.domain.order.model.dto.request;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public record HistoryControllerRequest(
+		@NotNull
+		@Positive
+		Long orderId,
+		@NotNull
+		@Positive
+		Long offerId
+) {
+}
