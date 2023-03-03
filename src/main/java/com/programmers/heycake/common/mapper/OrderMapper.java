7ea56b4b27a937b1ff0d557bd2a4f3c84a07e316
bufflect <bndgc@naver.com>
2023-03-04 04:51:25 +0900

    Style: OrderMapper 수정  - Collector.toList -> toList로 변경

--- src/main/java/com/programmers/heycake/common/mapper/OrderMapper.java
+++ src/main/java/com/programmers/heycake/common/mapper/OrderMapper.java
@@ -1,27 +1,9 @@
 package com.programmers.heycake.common.mapper;
 
-<<<<<<< HEAD
-import static com.programmers.heycake.common.util.AuthenticationUtil.*;
-import static com.programmers.heycake.domain.order.model.vo.OrderStatus.*;
 import static lombok.AccessLevel.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
-import com.programmers.heycake.domain.image.model.dto.ImageResponse;
-import com.programmers.heycake.domain.image.model.dto.ImageResponses;
-import com.programmers.heycake.domain.member.model.dto.response.OrderGetDetailResponse;
-import com.programmers.heycake.domain.order.model.dto.OrderDto;
-import com.programmers.heycake.domain.order.model.dto.request.OrderCreateRequest;
-import com.programmers.heycake.domain.order.model.dto.response.MyOrderResponse;
-import com.programmers.heycake.domain.order.model.dto.response.MyOrderResponseList;
-import com.programmers.heycake.domain.order.model.dto.response.OrderGetDetailServiceResponse;
-import com.programmers.heycake.domain.order.model.entity.CakeInfo;
-import com.programmers.heycake.domain.order.model.entity.Order;
-import com.programmers.heycake.domain.order.model.entity.OrderHistory;
-
-import static lombok.AccessLevel.*;
-
 import java.util.stream.Collectors;
 
 import com.programmers.heycake.domain.image.model.dto.ImageResponse;
@@ -29,90 +11,12 @@ import com.programmers.heycake.domain.image.model.dto.ImageResponses;
 import com.programmers.heycake.domain.order.model.dto.response.OrderGetServiceSimpleResponse;
 import com.programmers.heycake.domain.order.model.dto.response.OrderGetSimpleResponse;
 import com.programmers.heycake.domain.order.model.entity.Order;
->>>>>>> dfbf285 (Feat: 주문 리스트 조회 기능 구현)
+import com.programmers.heycake.domain.order.model.entity.OrderHistory;
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public class OrderMapper {
-
-	public static Order toEntity(OrderDto orderDto) {
-		return Order.builder()
-				.memberId(orderDto.memberId())
-				.title(orderDto.title())
-				.orderStatus(orderDto.orderStatus())
-				.hopePrice(orderDto.hopePrice())
-				.region(orderDto.region())
-				.visitDate(orderDto.visitDate())
-				.cakeInfo(orderDto.cakeInfo())
-				.build();
-	}
-
-	public static Order toEntity(OrderCreateRequest orderCreateRequest, CakeInfo cakeInfo) {
-		return Order.builder()
-				.cakeInfo(cakeInfo)
-				.hopePrice(orderCreateRequest.hopePrice())
-				.memberId(getMemberId())
-				.orderStatus(NEW)
-				.visitDate(orderCreateRequest.visitTime())
-				.title(orderCreateRequest.title())
-				.region(orderCreateRequest.region())
-				.build();
-	}
-
-	public static OrderDto toOrderDto(Order order) {
-		return OrderDto.builder()
-				.id(order.getId())
-				.memberId(order.getMemberId())
-				.title(order.getTitle())
-				.orderStatus(order.getOrderStatus())
-				.hopePrice(order.getHopePrice())
-				.region(order.getRegion())
-				.visitDate(order.getVisitDate())
-				.cakeInfo(order.getCakeInfo())
-				.build();
-	}
-
-	public static OrderGetDetailServiceResponse toOrderGetDetailServiceResponse(Order order) {
-		return OrderGetDetailServiceResponse.builder()
-				.orderId(order.getId())
-				.memberId(order.getMemberId())
-				.title(order.getTitle())
-				.cakeInfo(order.getCakeInfo())
-				.orderStatus(order.getOrderStatus())
-				.visitDate(order.getVisitDate())
-				.hopePrice(order.getHopePrice())
-				.region(order.getRegion())
-				.offerCount(order.getOffers().size())
-				.createdAt(order.getCreatedAt())
-				.updatedAt(order.getUpdatedAt())
-				.build();
-	}
-
-	public static OrderGetDetailResponse toOrderGetDetailResponse(
-			OrderGetDetailServiceResponse orderGetDetailServiceResponse,
-			ImageResponses imageResponses
-	) {
-		return OrderGetDetailResponse.builder()
-				.orderId(orderGetDetailServiceResponse.orderId())
-				.memberId(orderGetDetailServiceResponse.memberId())
-				.title(orderGetDetailServiceResponse.title())
-				.region(orderGetDetailServiceResponse.region())
-				.orderStatus(orderGetDetailServiceResponse.orderStatus())
-				.hopePrice(orderGetDetailServiceResponse.hopePrice())
-				.visitDate(orderGetDetailServiceResponse.visitDate())
-				.cakeInfo(orderGetDetailServiceResponse.cakeInfo())
-				.offerCount(orderGetDetailServiceResponse.offerCount())
-				.images(imageResponses.images()
-						.stream()
-						.map(ImageResponse::imageUrl)
-						.toList()
-				)
-				.offerCount(orderGetDetailServiceResponse.offerCount())
-				.createdAt(orderGetDetailServiceResponse.createdAt())
-				.updatedAt(orderGetDetailServiceResponse.updatedAt())
-				.build();
-	}
 
 	public static OrderGetServiceSimpleResponse toOrderGetServiceSimpleResponse(Order order) {
 		return OrderGetServiceSimpleResponse.builder()
@@ -127,10 +31,26 @@ public class OrderMapper {
 				.build();
 	}
 
-	public static MyOrderResponseList toMyOrderResponseListForMember(
-			List<MyOrderResponse> orderList,
-			LocalDateTime lastTime) {
-		return new MyOrderResponseList(orderList, lastTime);
+	public static OrderGetSimpleResponse toOrderGetSimpleResponse(
+			OrderGetServiceSimpleResponse orderSimpleGetServiceResponse,
+			ImageResponses imageResponses
+	) {
+		return OrderGetSimpleResponse.builder()
+				.orderId(orderSimpleGetServiceResponse.orderId())
+				.title(orderSimpleGetServiceResponse.title())
+				.cakeInfo(orderSimpleGetServiceResponse.cakeInfo())
+				.orderStatus(orderSimpleGetServiceResponse.orderStatus())
+				.hopePrice(orderSimpleGetServiceResponse.hopePrice())
+				.images(imageResponses.images()
+						.stream()
+						.map(ImageResponse::imageUrl)
+						.collect(Collectors.toList())
+				)
+				.region(orderSimpleGetServiceResponse.region())
+				.visitTime(orderSimpleGetServiceResponse.visitDate())
+				.createdAt(orderSimpleGetServiceResponse.createdAt())
+				.build()
+				;
 	}
 
 	//TODO 추후에 삭제
@@ -167,4 +87,5 @@ public class OrderMapper {
 		//TODO 추후에 변경
 		return toMyOrderResponseListForMember(lastTime, orderList);
 	}
+
 }
