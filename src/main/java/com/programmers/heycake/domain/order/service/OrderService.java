716f92ee7197a337a8a2fecb2cb6cc5b7dff6203
bufflect <bndgc@naver.com>
2023-03-03 17:11:17 +0900

    Fix: EntityNotFound BusinessException으로 변경

--- src/main/java/com/programmers/heycake/domain/order/service/OrderService.java
+++ src/main/java/com/programmers/heycake/domain/order/service/OrderService.java
@@ -2,11 +2,11 @@ package com.programmers.heycake.domain.order.service;
 
 import static com.programmers.heycake.common.mapper.OrderMapper.*;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.programmers.heycake.common.exception.BusinessException;
+import com.programmers.heycake.common.exception.ErrorCode;
 import com.programmers.heycake.domain.order.dto.response.OrderGetDetailServiceResponse;
 import com.programmers.heycake.domain.order.model.entity.Order;
 import com.programmers.heycake.domain.order.repository.OrderRepository;
@@ -21,7 +21,7 @@ public class OrderService {
 	@Transactional(readOnly = true)
 	public OrderGetDetailServiceResponse getOrder(Long orderId) {
 		Order order = orderRepository.findById(orderId)
-				.orElseThrow(EntityNotFoundException::new);
+				.orElseThrow(() -> new BusinessException(ErrorCode.ENTITY_NOT_FOUND));
 		return toOrderGetServiceDetailResponse(order);
 	}
 }
