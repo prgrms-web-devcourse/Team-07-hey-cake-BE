517a8ebd143052aae272a0191fa6680bd6522f2b
bufflect <bndgc@naver.com>
2023-03-03 16:32:51 +0900

    Style: CreamFlavor 값 하나에 행 하나 오도록 수정

--- src/main/java/com/programmers/heycake/domain/order/model/vo/CreamFlavor.java
+++ src/main/java/com/programmers/heycake/domain/order/model/vo/CreamFlavor.java
@@ -1,5 +1,9 @@
 package com.programmers.heycake.domain.order.model.vo;
 
 public enum CreamFlavor {
-	WHIPPED_CREAM, CREAM_CHEESE, CHOCO, OREO, ETC
+	WHIPPED_CREAM,
+	CREAM_CHEESE,
+	CHOCO,
+	OREO,
+	ETC
 }
