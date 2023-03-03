53fecbf8f6201cbc1bbc4e83d16952aa423e4fc9
goseungwon <knmy0925@gmail.com>
2023-03-03 18:23:41 +0900

    Add: Dto 생성

---
+++ src/main/java/com/programmers/heycake/domain/order/model/dto/response/MyOrderResponseList.java
@@ -0,0 +1,15 @@
+package com.programmers.heycake.domain.order.model.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public record MyOrderResponseList(
+		//TODO 나중에 변경
+		List<MyOrderResponse> myOrderResponseList,
+		// List<OrderDtoWithImage> myOrderResponseList,
+		@JsonFormat(pattern = "yyyy-MM-ddTHH:mm:ss", timezone = "Asia/Seoul")
+		LocalDateTime lastTime
+) {
+}
