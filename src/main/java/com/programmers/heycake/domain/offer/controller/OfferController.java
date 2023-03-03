cd18c21e6b20bcc5e41cf6e82e1e00b3aeb12a16
Kong <rhdtn311@naver.com>
2023-03-03 14:52:43 +0900

    Chore: 통일된 uri @RequestMapping 으로 공통화

--- src/main/java/com/programmers/heycake/domain/offer/controller/OfferController.java
+++ src/main/java/com/programmers/heycake/domain/offer/controller/OfferController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +20,13 @@ import com.programmers.heycake.domain.offer.model.dto.response.OfferSummaryRespo
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/api/v1/offers")
 @RequiredArgsConstructor
 public class OfferController {
 
 	private final OfferFacade offerFacade;
 
-	@PostMapping("/api/v1/offers")
+	@PostMapping
 	public ResponseEntity<Void> saveOffer(@ModelAttribute OfferSaveRequest offerSaveRequest,
 			@RequestParam Long memberId) {
 
@@ -32,7 +34,7 @@ public class OfferController {
 		return ResponseEntity.created(URI.create("/api/v1/offers/" + savedOfferId)).build();
 	}
 
-	@GetMapping("/api/v1/offers")
+	@GetMapping
 	public ResponseEntity<List<OfferSummaryResponse>> getOffers(@RequestBody OfferSummaryRequest offerSummaryRequest) {
 
 		return ResponseEntity.ok(offerFacade.getOffers(offerSummaryRequest));
