a1ab19d369b6da7ffab7d9498740ce1529695c2f
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-03-03 14:54:30 +0900

    Feat: 제안(스레드) 조회 기능 구현 (#92)

--- src/main/java/com/programmers/heycake/domain/offer/controller/OfferController.java
+++ src/main/java/com/programmers/heycake/domain/offer/controller/OfferController.java
@@ -1,16 +1,21 @@
 package com.programmers.heycake.domain.offer.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.programmers.heycake.domain.offer.facade.OfferFacade;
 import com.programmers.heycake.domain.offer.model.dto.request.OfferSaveRequest;
+import com.programmers.heycake.domain.offer.model.dto.request.OfferSummaryRequest;
+import com.programmers.heycake.domain.offer.model.dto.response.OfferSummaryResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,5 +32,11 @@ public class OfferController {
 
 		Long savedOfferId = offerFacade.saveOffer(offerSaveRequest, memberId);
 		return ResponseEntity.created(URI.create("/api/v1/offers/" + savedOfferId)).build();
+	}
+
+	@GetMapping
+	public ResponseEntity<List<OfferSummaryResponse>> getOffers(@RequestBody OfferSummaryRequest offerSummaryRequest) {
+
+		return ResponseEntity.ok(offerFacade.getOffers(offerSummaryRequest));
 	}
 }
