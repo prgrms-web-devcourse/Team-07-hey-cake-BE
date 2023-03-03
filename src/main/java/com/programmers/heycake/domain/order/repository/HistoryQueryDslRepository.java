2d1c4d6d44be44c286509942d6fbb6ff4e90badc
goseungwon <knmy0925@gmail.com>
2023-03-03 18:23:06 +0900

    Add: queryDsl repository 생성

---
+++ src/main/java/com/programmers/heycake/domain/order/repository/HistoryQueryDslRepository.java
@@ -0,0 +1,47 @@
+package com.programmers.heycake.domain.order.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.programmers.heycake.domain.order.model.entity.OrderHistory;
+import com.programmers.heycake.domain.order.model.entity.QOrderHistory;
+import com.programmers.heycake.domain.order.model.vo.OrderStatus;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class HistoryQueryDslRepository {
+	private final JPAQueryFactory jpaQueryFactory;
+
+	QOrderHistory qOrderHistory = QOrderHistory.orderHistory;
+
+	public List<OrderHistory> findAllByMarketIdOrderByVisitDateAsc(
+			Long memberId,
+			String option,
+			LocalDateTime cursorTime,
+			int pageSize) {
+		return jpaQueryFactory
+				.selectFrom(qOrderHistory)
+				.where(
+						gtOrderTime(cursorTime),
+						orderStatus(option),
+						qOrderHistory.marketId.eq(memberId)
+				).orderBy(qOrderHistory.order.visitDate.asc())
+				.limit(pageSize)
+				.fetch();
+
+	}
+
+	private BooleanExpression gtOrderTime(LocalDateTime cursorTime) {
+		return cursorTime == null ? null : qOrderHistory.order.visitDate.gt(cursorTime);
+	}
+
+	private BooleanExpression orderStatus(String option) {
+		return option == null ? null : qOrderHistory.order.orderStatus.eq(OrderStatus.valueOf(option));
+	}
+}
---
+++ src/main/java/com/programmers/heycake/domain/order/repository/OrderQueryDslRepository.java
@@ -0,0 +1,143 @@
+package com.programmers.heycake.domain.order.repository;
+
+import static com.programmers.heycake.domain.image.model.vo.ImageType.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.programmers.heycake.domain.image.model.entity.QImage;
+import com.programmers.heycake.domain.order.model.dto.response.MyOrderResponse;
+import com.programmers.heycake.domain.order.model.entity.QOrder;
+import com.programmers.heycake.domain.order.model.vo.OrderStatus;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderQueryDslRepository {
+	private final JPAQueryFactory jpaQueryFactory;
+	QOrder qOrder = QOrder.order;
+	QImage qImage = QImage.image;
+
+	// public List<Order> findAllByMemberIdOrderByVisitDateAsc(Long memberId, String option, LocalDateTime cursorTime,
+	// 		int pageSize) {
+	// 	return jpaQueryFactory
+	// 			.selectFrom(qOrder)
+	// 			.where(
+	// 					gtOrderTime(cursorTime),
+	// 					orderStatus(option),
+	// 					qOrder.memberId.eq(memberId)
+	// 			).orderBy(qOrder.visitDate.asc())
+	// 			.limit(pageSize)
+	// 			.fetch();
+	// }
+
+	public List<MyOrderResponse> findAllByMemberIdOrderByVisitDateAsc(Long memberId, String option,
+			LocalDateTime cursorTime,
+			int pageSize) {
+
+		// List<String> imageUrls = jpaQueryFactory.selectFrom(qImage)
+		// 		.where(
+		// 				qImage.referenceId.eq(qOrder.id),
+		// 				qImage.imageType.eq(ORDER)
+		// 		).stream()
+		// 		.map(Image::getImageUrl)
+		// 		.toList();
+
+		return jpaQueryFactory
+				.select(
+						Projections.constructor(
+								MyOrderResponse.class,
+								qOrder.id,
+								qOrder.title,
+								qOrder.orderStatus,
+								qOrder.region,
+								qOrder.visitDate,
+								qOrder.createdAt,
+								qImage.imageUrl
+						)
+				)
+				.from(qOrder)
+				.leftJoin(qImage)
+				.on(
+						qOrder.id.eq(qImage.referenceId).and(qImage.imageType.eq(ORDER))
+				).limit(1)
+				.where(
+						gtOrderTime(cursorTime),
+						orderStatus(option),
+						qOrder.memberId.eq(memberId)
+				).orderBy(qOrder.visitDate.asc())
+				.limit(pageSize)
+				.fetch();
+
+		// List<Tuple> tuples = jpaQueryFactory
+		// 		.select(qOrder, qImage.imageUrl)
+		// 		.from(qOrder)
+		// 		.leftJoin(qImage)
+		// 		.on(qOrder.id.eq(qImage.referenceId),
+		// 				qImage.imageType.eq(ORDER))
+		// 		.select(qOrder, qImage.imageUrl)
+		// 		.from(qOrder)
+		// 		.where(
+		// 				qOrder.id.eq(memberId),
+		// 				gtOrderTime(cursorTime),
+		// 				orderStatus(option)
+		// 		).orderBy(qOrder.visitDate.asc())
+		// 		.limit(pageSize)
+		// 		.fetch();
+		//
+		// return tuples.stream()
+		// 		.distinct()
+		// 		.map(
+		// 				tuple -> MyOrderResponse.builder()
+		// 						.id(tuple.get(qOrder.id))
+		// 						.title(tuple.get(qOrder.title))
+		// 						.orderStatus(tuple.get(qOrder.orderStatus))
+		// 						.region(tuple.get(qOrder.region))
+		// 						.visitTime(tuple.get(qOrder.visitDate))
+		// 						.createdAt(tuple.get(qOrder.createdAt))
+		// 						.imageUrl(tuple.get(qImage.imageUrl))
+		// 						.build()
+		// 		).toList();
+
+		//TODO 리팩토링하기
+		// jpaQueryFactory.selectDistinct(qOrder, qImage.imageUrl)
+		// 		.from(qOrder)
+		// 		.leftJoin(
+		// 				select(qImage.referenceId, qImage.imageUrl)
+		// 						.from(qImage)
+		// 						.where(qImage.imageType.eq(ORDER))
+		// 						.groupBy(qImage.referenceId, qImage.imageUrl)
+		// 						.limit(1), qImage)
+		// 		.on(qOrder.id.eq(qImage.referenceId))
+		// 		.fetch();
+
+		// return jpaQueryFactory
+		// 		.select(qOrder, qImage.imageUrl)
+		// 		.from(qOrder)
+		// 		.leftJoin(
+		// 				JPAExpressions
+		// 						.select(qImage.referenceId, qImage.imageUrl)
+		// 						.from(qImage)
+		// 						.where(qImage.imageType.eq(ORDER))
+		// 						.groupBy(qImage.referenceId, qImage.imageUrl)
+		// 						.limit(1),
+		// 				on(qOrder.id.eq(qImage.referenceId))
+		// 		)
+		// 		.where(qOrder.id.eq(1L))
+		// 		.fetch();
+	}
+
+	private BooleanExpression gtOrderTime(LocalDateTime cursorTime) {
+		return cursorTime == null ? null : qOrder.visitDate.gt(cursorTime);
+	}
+
+	private BooleanExpression orderStatus(String option) {
+		return option == null ? null : qOrder.orderStatus.eq(OrderStatus.valueOf(option));
+	}
+}
