1ebf52bbf95a3fb42724a22030531a8579fb1354
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-03-05 04:39:31 +0900

    Feat: 댓글 삭제 기능 구현 (#110)

--- src/main/java/com/programmers/heycake/domain/comment/controller/CommentController.java
+++ src/main/java/com/programmers/heycake/domain/comment/controller/CommentController.java
@@ -3,7 +3,9 @@ package com.programmers.heycake.domain.comment.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,15 @@ import lombok.RequiredArgsConstructor;
 public class CommentController {
 
 	private final CommentFacade commentFacade;
-	
+
+	@DeleteMapping("/{commentId}")
+	public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+		commentFacade.deleteComment(commentId);
+
+		return ResponseEntity.noContent()
+				.build();
+	}
+
 	@GetMapping
 	public ResponseEntity<List<CommentSummaryResponse>> getComments(@RequestBody CommentsRequest commentsRequest) {
 
