0771892f54d5ad3229998a8df7b5b9a980ccf327
bufflect <bndgc@naver.com>
2023-03-05 14:22:35 +0900

    Fix : CustomDeniedHandler 수정   - errorResponse time 직렬화 실패 해결   - errorResponse path에 uri 추가

--- src/main/java/com/programmers/heycake/common/exception/CustomAccessDeniedHandler.java
+++ src/main/java/com/programmers/heycake/common/exception/CustomAccessDeniedHandler.java
@@ -1,17 +1,19 @@
 package com.programmers.heycake.common.exception;
 
+import static org.springframework.http.MediaType.*;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Collections;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.programmers.heycake.common.dto.ErrorResponse;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
@@ -20,16 +22,25 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException {
 
-		ErrorResponse errorResponse = ErrorResponse.of("권한이 없습니다.", "path", Collections.emptyList());
+		ErrorResponse errorResponse = ErrorResponse.of(
+				"권한이 없습니다.",
+				request.getRequestURI(),
+				null
+		);
 
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(APPLICATION_JSON.toString());
 		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-		OutputStream outputStream = response.getOutputStream();
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.writeValue(outputStream, errorResponse);
+		PrintWriter writer = response.getWriter();
 
-		outputStream.flush();
-		outputStream.close();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.writeValueAsString(errorResponse);
+
+		writer.println(errorResponse);
+		writer.flush();
+		writer.close();
 	}
 }
