26c536ee0bfd32919492cc767b21724f2ea81574
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-03-05 17:00:13 +0900

    Fix: OAuth 로그인 관련 수정 (#121)

--- src/main/java/com/programmers/heycake/domain/member/controller/MemberController.java
+++ src/main/java/com/programmers/heycake/domain/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.programmers.heycake.domain.member.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -22,18 +24,24 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+
+	private static final String LOGIN_DONE_REDIRECT_URL = "http://localhost:3000/main";
+
 	private final MemberService memberService;
 
-	@GetMapping("/login/oauth2/code/kakao")
-	public ResponseEntity<String> getAuthorizationCode(
+	@GetMapping("/login")
+	public void login(
 			@RequestParam String code, HttpServletResponse response
-	) {
+	) throws IOException {
+		log.info("login start: {}", code);
+
 		TokenResponse tokenResponse = memberService.loginForKakao(code);
-		Cookie accessToken = new Cookie("access_token", tokenResponse.token());
-		accessToken.setPath("/");
-		accessToken.setHttpOnly(true);
-		response.addCookie(accessToken);
-		return ResponseEntity.ok(tokenResponse.refreshToken());
+
+		response.setHeader("access_token", tokenResponse.accessToken());
+		response.setHeader("refresh_token", tokenResponse.refreshToken());
+		response.sendRedirect(LOGIN_DONE_REDIRECT_URL);
+
+		log.info("login end: {}", code);
 	}
 
 	@PostMapping("api/v1/members/refresh")
@@ -42,7 +50,7 @@ public class MemberController {
 			HttpServletResponse response
 	) {
 		TokenResponse tokenResponse = memberService.reissueToken(tokenRefreshRequest.refreshToken());
-		Cookie accessToken = new Cookie("access_token", tokenResponse.token());
+		Cookie accessToken = new Cookie("access_token", tokenResponse.accessToken());
 		accessToken.setPath("/");
 		accessToken.setHttpOnly(true);
 		response.addCookie(accessToken);
