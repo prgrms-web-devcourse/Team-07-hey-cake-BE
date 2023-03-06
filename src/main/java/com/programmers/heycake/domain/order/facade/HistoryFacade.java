de09581a4403ce1f3c4e9f591fcab00fc0e657b1
goseungwon <knmy0925@gmail.com>
2023-03-06 20:33:26 +0900

    Fix: 주문 확정 오류 해결

--- src/main/java/com/programmers/heycake/domain/order/facade/HistoryFacade.java
+++ src/main/java/com/programmers/heycake/domain/order/facade/HistoryFacade.java
@@ -3,11 +3,13 @@ package com.programmers.heycake.domain.order.facade;
 import static com.programmers.heycake.common.util.AuthenticationUtil.*;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.programmers.heycake.domain.offer.model.dto.OfferDto;
 import com.programmers.heycake.domain.offer.service.OfferService;
 import com.programmers.heycake.domain.order.model.dto.request.HistoryControllerRequest;
 import com.programmers.heycake.domain.order.model.dto.request.HistoryFacadeRequest;
+import com.programmers.heycake.domain.order.model.entity.Order;
 import com.programmers.heycake.domain.order.model.vo.OrderStatus;
 import com.programmers.heycake.domain.order.service.HistoryService;
 import com.programmers.heycake.domain.order.service.OrderService;
@@ -21,11 +23,12 @@ public class HistoryFacade {
 	private final OfferService offerService;
 	private final OrderService orderService;
 
+	@Transactional
 	public Long createHistory(HistoryControllerRequest historyControllerRequest) {
 		orderService.updateOrderState(historyControllerRequest.orderId(), OrderStatus.RESERVED);
 		OfferDto offerDto = offerService.getOfferById(historyControllerRequest.offerId());
-		HistoryFacadeRequest historyFacadeRequest =
-				new HistoryFacadeRequest(getMemberId(), offerDto.marketId(), offerDto.orderDto());
+		Order order = orderService.getOrder(offerDto.orderDto().id());
+		HistoryFacadeRequest historyFacadeRequest = new HistoryFacadeRequest(getMemberId(), offerDto.marketId(), order);
 
 		return historyService.createHistory(historyFacadeRequest);
 	}
