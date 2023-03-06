02bb0c24dca72e8f67686d7357b55d2e10ae5b71
knmy0925@gmail.com <85796588+goseungwon@users.noreply.github.com>
2023-03-06 16:51:52 +0900

    Refactor: 주문내역조회 image 첫번째만 조인하도록 구현 (#127)
    Co-authored-by: JuHyeong <86050295+BnDC@users.noreply.github.com>
    Co-authored-by: rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
    Co-authored-by: Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>

--- src/main/java/com/programmers/heycake/domain/order/model/dto/response/MyOrderResponseList.java
+++ src/main/java/com/programmers/heycake/domain/order/model/dto/response/MyOrderResponseList.java
@@ -6,9 +6,8 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record MyOrderResponseList(
-		//TODO 나중에 변경
 		List<MyOrderResponse> myOrderResponseList,
-		@JsonFormat(pattern = "yyyy-MM-ddTHH:mm:ss", timezone = "Asia/Seoul")
-		LocalDateTime lastCursorTime
+		@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+		LocalDateTime lastCursorDate
 ) {
 }
