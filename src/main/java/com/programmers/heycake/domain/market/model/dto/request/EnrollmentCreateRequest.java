ca714775594c9d22c04bfe12a03b4f26710e921a
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-03-08 13:43:07 +0900

    Fix: Request DTO 에 Length 관련 검증 추가 (#158)

--- src/main/java/com/programmers/heycake/domain/market/model/dto/request/EnrollmentCreateRequest.java
+++ src/main/java/com/programmers/heycake/domain/market/model/dto/request/EnrollmentCreateRequest.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,17 +14,17 @@ import lombok.Builder;
 
 @Builder
 public record EnrollmentCreateRequest(
-		@NotBlank String businessNumber,
-		@NotBlank String ownerName,
+		@NotBlank @Length(max = 10) String businessNumber,
+		@NotBlank @Length(max = 10) String ownerName,
 		@NotNull @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate openDate,
-		@NotBlank String marketName,
-		@NotBlank String phoneNumber,
-		@NotBlank String city,
-		@NotBlank String district,
-		@NotBlank String detailAddress,
+		@NotBlank @Length(max = 20) String marketName,
+		@NotBlank @Length(max = 20) String phoneNumber,
+		@NotBlank @Length(max = 10) String city,
+		@NotBlank @Length(max = 10) String district,
+		@NotBlank @Length(max = 80) String detailAddress,
 		@NotNull @DateTimeFormat(pattern = "HH:mm") LocalTime openTime,
 		@NotNull @DateTimeFormat(pattern = "HH:mm") LocalTime endTime,
-		@NotBlank String description,
+		@NotBlank @Length(max = 500) String description,
 		@NotNull MultipartFile businessLicenseImage,
 		@NotNull MultipartFile marketImage
 ) {
