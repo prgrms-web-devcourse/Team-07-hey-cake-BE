68fc070ae48bcdd9c507ed1eebc265c0059c984a
goseungwon <knmy0925@gmail.com>
2023-03-10 02:10:29 +0900

    Refactor: order/my 권한 필터 적용하도록 변경

--- src/main/java/com/programmers/heycake/domain/order/controller/OrderController.java
+++ src/main/java/com/programmers/heycake/domain/order/controller/OrderController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -62,6 +63,7 @@ public class OrderController {
 	}
 
 	@GetMapping("/my")
+	@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_MARKET')")
 	public ResponseEntity<MyOrderResponseList> getOrderList(
 			@RequestParam(required = false) @Positive Long cursorId,
 			@RequestParam(defaultValue = "10") @Positive Integer pageSize,
