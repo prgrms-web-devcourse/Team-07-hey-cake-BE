38a5142aa28a11769fd3786a336a799cba1c97aa
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2023-03-13 16:47:06 +0900

    Chore: 댓글 목록 조회 시 생성 시간 반환 (#252)

--- src/main/java/com/programmers/heycake/domain/comment/model/dto/response/CommentSummaryResponse.java
+++ src/main/java/com/programmers/heycake/domain/comment/model/dto/response/CommentSummaryResponse.java
@@ -1,7 +1,10 @@
 package com.programmers.heycake.domain.comment.model.dto.response;
 
+import java.time.LocalDateTime;
+
 import lombok.Builder;
 
 @Builder
-public record CommentSummaryResponse(Long commentId, String comment, String image, Long memberId, String nickname) {
+public record CommentSummaryResponse(Long commentId, String comment, String image, Long memberId, String nickname,
+																		 LocalDateTime createdAt) {
 }
